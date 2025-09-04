bf5c4e44225aeb96ddb59f4a0ce7d098aea74ffa
Ozystar117 <o.emmanuelozioma@gmail.com>
2025-09-04 22:15:58 +0100

    rotate player with joystick

--- lib/components/birds/flappy_bird.dart
+++ lib/components/birds/flappy_bird.dart
@@ -1,16 +1,20 @@
 import 'dart:async';
+import 'dart:math';
 
+import 'package:flame/components.dart';
 import 'package:flame/sprite.dart';
 import 'package:flappy_action/components/birds/bird.dart';
 import 'package:flappy_action/util/sprite_util.dart';
 
 class FlappyBird extends Bird {
   late final List<Sprite> sprites;
+  late final JoystickComponent joystick;
 
   FlappyBird({
     required super.size,
     required super.position,
     required super.animationTime,
+    required this.joystick,
   });
 
   @override
@@ -21,5 +25,17 @@ class FlappyBird extends Bird {
       SpriteUtil.yellowBird3,
     ];
     super.onLoad();
+  }
+
+  @override
+  update(double dt) {
+    // Update rotation based on joystick
+    if (joystick.delta.length > 0.1) {
+      angle = atan2(joystick.delta.y, joystick.delta.x);
+    } else {
+      angle = 0;
+    }
+
+    super.update(dt);
   }
 }
